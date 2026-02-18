87fb4fb1a398ba06bf0dd910497fad3b4ded4268
Vimukthi pramudantha <vimukthipramudanth@gmail.com>
2026-02-18 14:49:00 +0530

    401

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -23,7 +23,7 @@ export const registerAdmin = async (req, res) => {
     res.cookie('jwt', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
 
@@ -60,7 +60,7 @@ export const loginUser = async (req, res) => {
   res.cookie('jwt', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: 7 * 24 * 60 * 60 * 1000
   });
 

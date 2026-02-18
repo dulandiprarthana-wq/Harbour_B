65d9631a0624d9194470b81684f4ed6d210d12bf
Vimukthi pramudantha <vimukthipramudanth@gmail.com>
2026-02-18 14:37:22 +0530

    Update

--- src/server.js
+++ src/server.js
@@ -20,10 +20,10 @@ const __dirname = path.dirname(__filename);
 // Middleware
 app.use(cors({
   origin: [
-    'http://localhost:5173',
-    'http://localhost:5173/',
-    'https://leafy-lebkuchen-d01b7a.netlify.app',
+    'http://localhost:5174',
+    'http://localhost:5174/',
     'https://leafy-lebkuchen-d01b7a.netlify.app/',
+    'https://leafy-lebkuchen-d01b7a.netlify.app',
     'https://transcorp-canada.netlify.app'
   ],
   credentials: true

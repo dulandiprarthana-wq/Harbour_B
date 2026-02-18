b5c8009307c3444affe92fabe612a3650404b8ec
Vimukthi pramudantha <vimukthipramudanth@gmail.com>
2026-02-18 10:03:52 +0530

    bug fixed and added Transcorp cors

--- src/server.js
+++ src/server.js
@@ -19,7 +19,8 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:3000',
-    'https://leafy-lebkuchen-d01b7a.netlify.app'
+    'https://leafy-lebkuchen-d01b7a.netlify.app',
+    'https://transcorp-canada.netlify.app/'
   ],
   credentials: true
 }));
